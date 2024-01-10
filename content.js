fd73906ac5ed21c9393a3bac0cf3600e881b69db
David Warshawsky <davidawarshawsky@gmail.com>
2024-01-10 15:13:08 -0800

    added icons and icon generation script

---
+++ content.js
@@ -0,0 +1,19 @@
+// var button = document.querySelector('.jobs-apply-button.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view');
+// if (button) {
+//   button.click();
+//   setTimeout(() => {
+  // setTimeout(() => {
+  //   var button2 = document.querySelector('button[aria-label="Continue to next step"]');
+  //   if (button2) {
+  //     button2.click();
+  //   } else {
+  //     console.log('button2 not found');
+  //   }
+  // }, 2142);
+  
+let imageURL = chrome.runtime.getURL('images/logo.png');
+
+  // Use the imageURL as needed in your content script
+console.log('Image URL:', imageURL);
+// class="artdeco-button artdeco-button--2 artdeco-button--primary ember-view"
+// .artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view
