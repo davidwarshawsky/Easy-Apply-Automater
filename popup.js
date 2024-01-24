ac08e4b3d0a3185b6bc982ec3e1198d79a14877d
David Warshawsky <davidawarshawsky@gmail.com>
2024-01-24 10:05:47 -0800

    Removed getRadioQuestions and moved everything to one function called extractQuestions because that is the purpose of this branch. It doesn't yet save the questions as I want to make sure I have all question versions selected before I save them. It pops up for text input questions where it asks for a simple number in one line, dropdown questions and radio questions and then fills them in correspondingly to that question. I have placed too much work in this branch so I am commiting it and then moving it to merge into main. Then I will add each feature as a separate branch"

--- popup.js
+++ popup.js
@@ -141,12 +141,6 @@ function clickButtonOnPage() {
   }
 
   function removeAppliedJobs() {
-    // jobs are inside of div class="jobs-search-results-list"
-    // job cards are described inside of the above div with div.job-card-container--clickable
-    // the job card's status is described in the above div in ul class="job-card-list__footer-wrapper" and then 
-    // li class="job-card-container__footer-job-state" and the text is "Applied"
-    // if the job card is applied, then the job card is removed from the page
-    // if the job card is not applied, then the job card is not removed from the page
     var jobsList = document.querySelector('div.jobs-search-results-list');
     if (jobsList) {
       var jobCards = jobsList.querySelectorAll('div.job-card-container--clickable');
@@ -197,98 +191,159 @@ function clickButtonOnPage() {
     }
   }
 
-
-  function getRadioQuestions() {
-    return new Promise((resolve, reject) => {
-      var radioQuestionsDivs = document.querySelectorAll('div.jobs-easy-apply-form-section__grouping');
-      if (radioQuestionsDivs) {
-        for (const radioQuestionDiv of radioQuestionsDivs) {
-          var questionCSS = 'fieldset legend > span.fb-dash-form-element__label > span[aria-hidden="true"]';
-          var question = radioQuestionDiv.querySelector(questionCSS);
-          if (question) {
-            var questionText = question.textContent.trim();
-            console.log(questionText);
-          }
-          //   // get the radio buttons
-          //   var radioButtons = radioQuestionDiv.querySelectorAll('input[type="radio"]');
-          //   // for each radio button
-          //   for (const radioButton of radioButtons) {
-          //     // get the label
-          //     var label = radioButton.querySelector('label');
-          //     if (label) {
-          //       var labelText = label.textContent.trim();
-          //       console.log(labelText);
-          //     }
-          //   }
-        }
-        resolve();
-      } else {
-        // reject(new Error('Radio questions not found'));
-        resolve();
-      }
-    });
-  }
-
   function extractQuestions() {
-    // Note race/ethnicity question is not extracted.
-    // span.inline-block.t-14.t-bold.mt4 "Race/Ethnicity*""
-    // div[data-test-text-selectable-option="0"] > input[data-test-text-selectable-option__input="I consent"]
-    // we want to go through each grouping var questionDivs = document.querySelectorAll('div.jobs-easy-apply-form-section__grouping');
     var questionDivs = document.querySelectorAll('div.jobs-easy-apply-form-section__grouping');
+    // div.fb-dash-form-element[data-test-form-element] label[for^="single-typeahead-entity-form-component"]
+//     <div id="single-typeahead-entity-form-component-formElement-urn-li-jobs-applyformcommon-easyApplyFormElement-3788477971-9054483911573926553-city-HOME-CITY-ta" class="search-basic-typeahead search-vertical-typeahead">
+// <!---->        
     
-    // Extract questions from legend elements
-    var legendQuestions = document.querySelectorAll('legend span[data-test-form-builder-radio-button-form-component__title] span[aria-hidden="true"]');
-    legendQuestions.forEach(function (legendQuestion) {
-      console.log('Legend Question:', legendQuestion.textContent.trim());
-    });
+//     <input id="single-typeahead-entity-form-component-formElement-urn-li-jobs-applyformcommon-easyApplyFormElement-3788477971-9054483911573926553-city-HOME-CITY" required="" role="combobox" aria-autocomplete="list" aria-activedescendant="" aria-expanded="false" type="text" aria-describedby="single-typeahead-entity-form-component-formElement-urn-li-jobs-applyformcommon-easyApplyFormElement-3788477971-9054483911573926553-city-HOME-CITY-error">
   
-    // Extract questions from text entity list form components (dropdowns)
+// <!---->  
+//     </div>
     questionDivs.forEach(function (questionDiv) {
-      // Extract questions from single-line text form components
-      var textInputElements = questionDiv.querySelectorAll('div.fb-dash-form-element[data-test-form-element] label[for^="single-line-text-form-component"]');
-      textInputElements.forEach(function (textInputElement) {
+      // handles text input questions
+      var textInputElement = questionDiv.querySelector('div.fb-dash-form-element[data-test-form-element] label[for^="single-line-text-form-component"]');
+      if (textInputElement) {
         var labelText = textInputElement.textContent.trim();
         console.log('Text Input Question:', labelText);
-        var inputBox = document.querySelector('input.artdeco-text-input--input');
+        var inputBox = questionDiv.querySelector('input.artdeco-text-input--input');
         if (inputBox) {
           var inputValue = inputBox.value;
-          if (inputValue === '') {
+          if (inputValue === '' || inputValue === null) {
             var newInputValue = prompt('Please enter a value:\n' + labelText);
-            if (newInputValue !== null) {
-              inputBox.value = newInputValue;
-            }
+            inputBox.value = parseInt(newInputValue, 10);
+            // Trigger input event to simulate user input
+            var inputEvent = new Event('input', { bubbles: true });
+            inputBox.dispatchEvent(inputEvent);
+          }
           }
           console.log('Current Value:', inputBox.value);
-        }
-      });
-      var dropdownLabels = questionDiv.querySelectorAll('.fb-dash-form-element[data-test-form-element] label[for^="text-entity-list-form-component"]');
-      if (dropdownLabels.length > 0) {
-        dropdownLabels.forEach(function (label) {
-          var labelText = label.textContent.trim();
-          // Split the string in half based on the length
-          var splitIndex = Math.floor(labelText.length / 2);
-          var labelText = labelText.substring(0, splitIndex).trim();
-      
-          console.log('Dropdown Question:', labelText);
-          var multipleChoiceSelector = questionDiv.querySelector('select');
-          if (multipleChoiceSelector) {
-            var multipleChoiceOptions = multipleChoiceSelector.querySelectorAll('option');
-            multipleChoiceOptions.forEach(function (multipleChoiceOption) {
-              var multipleChoiceOptionText = multipleChoiceOption.textContent.trim();
-              console.log('Multiple Choice Option:', multipleChoiceOptionText);
-              if (multipleChoiceOption.selected) {
-                console.log('Selected Option:', multipleChoiceOptionText);
+      }
+      // handles radio questions
+      var fieldset = questionDiv.querySelector('fieldset');
+      if (fieldset) {
+        var legendElement = fieldset.querySelector('legend span[aria-hidden="true"]');
+        if (legendElement) {
+          var textContent = legendElement.textContent.trim();
+          console.log('Radio Question:', textContent);
+          var inputElements = fieldset.querySelectorAll('input[data-test-text-selectable-option__input]');
+          var isAnyInputSelected = false;
+          inputElements.forEach(function (input) {
+            console.log(textContent + "\noption: ", input.getAttribute('data-test-text-selectable-option__input'));
+            if (input.checked) {
+              isAnyInputSelected = true;
+            }
+          });
+  
+          if (!isAnyInputSelected) {
+            var options = [];
+            inputElements.forEach(function (input) {
+              options.push(input.getAttribute('data-test-text-selectable-option__input'));
+            });
+            // Create popup.html with question and options
+            var popupHTML = `
+              <h3>${textContent}</h3>
+              <ul>
+                ${options.map(option => `
+                  <li>
+                    <div style="border: 1px solid black; padding: 5px; margin-bottom: 5px;">
+                      <label>
+                        <input type="radio" name="${textContent}" value="${option}" />
+                        ${option}
+                      </label>
+                    </div>
+                  </li>
+                `).join('')}
+              </ul>
+            `;
+            // Open the popup.html in a new window
+            var popupWindow = window.open('', '', 'width=400,height=300');
+            popupWindow.document.write(popupHTML);
+            // Add event listener to handle option selection
+            popupWindow.document.addEventListener('click', function(event) {
+              var selectedInput = event.target.closest('input[type="radio"]');
+              if (selectedInput) {
+                var selectedOption = selectedInput.value;
+                inputElements.forEach(function (input) {
+                  if (input.getAttribute('data-test-text-selectable-option__input') === selectedOption) {
+                    input.checked = true;
+                    input.click(); // Trigger click event on the original input
+                  }
+                });
+                // Close the popup window
+                popupWindow.close();
               }
             });
           }
-        });
+        }
       }
+  
+      // handles dropdown questions
+      var dropdownLabels = questionDiv.querySelectorAll('.fb-dash-form-element[data-test-form-element] label[for^="text-entity-list-form-component"]');
+      dropdownLabels.forEach(function (label) {
+        var labelText = label.textContent.trim();
+        var splitIndex = Math.floor(labelText.length / 2);
+        labelText = labelText.substring(0, splitIndex).trim();
+        console.log('Dropdown Question:', labelText);
+        var multipleChoiceSelector = questionDiv.querySelector('select');
+        if (multipleChoiceSelector) {
+          var multipleChoiceOptions = multipleChoiceSelector.querySelectorAll('option');
+          var selectedOption = multipleChoiceSelector.value.toLowerCase();
+          if (selectedOption === 'select an option' || selectedOption === '') {
+            var options = [];
+            multipleChoiceOptions.forEach(function (multipleChoiceOption) {
+              var multipleChoiceOptionText = multipleChoiceOption.textContent.trim();
+              if (multipleChoiceOptionText.toLowerCase() !== 'select an option' && multipleChoiceOptionText !== '') {
+                options.push(multipleChoiceOptionText);
+              }
+            });
+            // Create popup.html with question and options
+            var popupHTML = `
+              <h3>${labelText}</h3>
+              <ul>
+                ${options.map(option => `
+                  <li>
+                    <div style="border: 1px solid black; padding: 5px; margin-bottom: 5px;" data-option="${option}">
+                      <label>
+                        <input type="radio" name="${labelText}" value="${option}" />
+                        ${option}
+                      </label>
+                    </div>
+                  </li>
+                `).join('')}
+              </ul>
+            `;
+            // Open the popup.html in a new window
+            var popupWindow = window.open('', '', 'width=400,height=300');
+            popupWindow.document.write(popupHTML);
+            // Add event listener to handle option selection
+            popupWindow.document.addEventListener('click', function(event) {
+              var selectedDiv = event.target.closest('div[data-option]');
+              if (selectedDiv) {
+                var selectedInput = selectedDiv.querySelector('input[type="radio"]');
+                if (selectedInput) {
+                  var selectedOption = selectedInput.value;
+                  multipleChoiceSelector.value = selectedOption;
+                  // Trigger change event on the original select element
+                  var changeEvent = new Event('change');
+                  multipleChoiceSelector.dispatchEvent(changeEvent);
+                  // Close the popup window
+                  popupWindow.close();
+                }
+              }
+            });
+          }
+        }
+      });
     });
   }
+  
   
 
   
   async function applyForJob() {
+    dismissJobsApplied();
     removeDismissedJobs();
     removeAppliedJobs();
     try {
@@ -298,7 +353,8 @@ function clickButtonOnPage() {
     }    
     while (true) {
       try {
-        await getRadioQuestions();
+        // await getRadioQuestions();
+        extractQuestions();
         await clickAndWait('button[aria-label="Continue to next step"]', 3000);
       } catch (error) {
         console.log('Continue button not found');
@@ -307,6 +363,7 @@ function clickButtonOnPage() {
     }
     while (true){
       try {
+        extractQuestions();
         await clickAndWait('button[aria-label="Review your application"]', 3000);
       } catch (error) {
         console.log('Review button not found');
@@ -325,8 +382,9 @@ function clickButtonOnPage() {
   function applyManyJobs() {
   }
 
-  // applyForJob();
+  applyForJob();
   // extractQuestions();
-  printRadioButtonQuestions();
+  // printRadioButtonQuestions();
+
   
 }
